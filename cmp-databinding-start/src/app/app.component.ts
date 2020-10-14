45def9aa7d00acf577414a3fbd3b08c37ccf9bb7
Dante De Craemer <dante.decraemer@stardekk.com>
2020-10-14 16:27:45 +0200

    until video 75

--- cmp-databinding-start/src/app/app.component.ts
+++ cmp-databinding-start/src/app/app.component.ts
@@ -6,23 +6,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  serverElements = [];
-  newServerName = '';
-  newServerContent = '';
+  serverElements = [{ type: 'server', name: 'testserver', content: 'just a test' }];
 
-  onAddServer() {
+  onServerAdded(serverData: { name: string, content: string }) {
     this.serverElements.push({
       type: 'server',
-      name: this.newServerName,
-      content: this.newServerContent
+      name: serverData.name,
+      content: serverData.content
     });
   }
 
-  onAddBlueprint() {
+  onBlueprintAdded(blueprintData: { name: string, content: string }) {
     this.serverElements.push({
       type: 'blueprint',
-      name: this.newServerName,
-      content: this.newServerContent
+      name: blueprintData.name,
+      content: blueprintData.content
     });
   }
+
 }
